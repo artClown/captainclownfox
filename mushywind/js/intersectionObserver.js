7263e19280f277672ae5a7e66958581acdb2fdc5
artClown <66511241+artClown@users.noreply.github.com>
2020-06-29 08:05:50 +0700

    update table, observer and index.html

--- mushywind/js/intersectionObserver.js
+++ mushywind/js/intersectionObserver.js
@@ -14,9 +14,9 @@ var qfObs = new IntersectionObserver(function (entries) {
     var qF = document.querySelector('#quick-fill');
     entries.forEach(function (entry) {
         if (entry.isIntersecting) {
-            var rowImage = entry.target,
-                srcImage = rowImage.getAttribute('data-src');
-            rowImage.src = srcImage;
+            // var rowImage = entry.target,
+            //     srcImage = rowImage.getAttribute('data-src');
+            // rowImage.src = srcImage;
             qF.style.display = 'none';
         } else {
             qF.style.display = 'block';
@@ -24,14 +24,29 @@ var qfObs = new IntersectionObserver(function (entries) {
     });
 });
 
+var qfPosMod = new IntersectionObserver(function(entries) {
+    var qF = document.querySelector('#quick-fill');
+    var footerHeight = document.getElementById('footer-wrapper').offsetHeight;
+    entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+            console.log('intersecting');
+            qF.style.bottom = footerHeight + 'px';
+        } else {
+            qF.style.bottom = 0;
+        }
+    });
+});
+
 var frm = (document.querySelector('#form'));
 var imgArr = (document.querySelectorAll('img.row-load'));
+var ftr = (document.querySelector('#footer-wrapper'));
 
 if (window.NodeList && !NodeList.prototype.forEach) {
     NodeList.prototype.forEach = Array.prototype.forEach;
 }
 
 qfObs.observe(frm);
+qfPosMod.observe(ftr);
 imgArr.forEach(function (v) {
     imageWatcher.observe(v);
 });
